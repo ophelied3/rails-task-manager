0733abc4cc94dc480c703f2441af37f620ec5560
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-08 13:33:39 +0200

    finished rails-task-manager

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808093632) do
+ActiveRecord::Schema.define(version: 20170808094140) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.date     "deadline"
-    t.boolean  "done"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "done",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
